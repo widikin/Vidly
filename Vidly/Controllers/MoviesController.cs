bf15052b4627fdff8e8a2e71f581b18e9c481239
widikin <digitsconnect@gmail.com>
2018-07-23 06:55:53 +0200

    Initial

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie
+            {
+                Id = 1,
+                Name = "Max Well"
+            };
+
+            return View();
+        }
+    }
+}
