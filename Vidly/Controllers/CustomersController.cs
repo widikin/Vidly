fa800cad00ddae34eea9de7ca50b1448366655ad
widikin <digitsconnect@gmail.com>
2018-08-01 05:54:30 +0200

    Loading Customers from Database

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -9,18 +9,42 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context= new ApplicationDbContext();
+        }
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = new List<Customer>{  new Customer{ Id = 1,Name = "William" }, new Customer {   Id = 2, Name = "Huguette"}, new Customer { Id = 3, Name = "Widikin" }};
+
+            var customers = _context.Customers.ToList();
+          //  var customers = GetCustomer();
             return View(customers);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
         }
 
         public ActionResult Details(int id)
         {
-            var customers = new List<Customer> { new Customer { Id = 1, Name = "William" }, new Customer { Id = 2, Name = "Huguette" }, new Customer { Id = 3, Name = "Widikin" } };
-            var customer = customers.SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
             return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomer()
+        {
+            return new List<Customer>
+            {
+                  new Customer{ Id = 1,Name = "William" },
+                  new Customer {   Id = 2, Name = "Huguette"},
+                  new Customer { Id = 3, Name = "Widikin" }
+            };
         }
     }
 }
