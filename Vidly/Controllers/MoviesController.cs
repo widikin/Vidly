dbef6d7eea6ce2e3343841b80117bffb940c25a8
widikin <digitsconnect@gmail.com>
2018-08-17 12:26:03 +0200

    Exercice-4 Movie Form add and Update

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -38,16 +38,61 @@ namespace Vidly.Controllers
             return View(viewModel);
         }
 
+   
         public ActionResult Edit(int id)
         {
-            return Content("id=" + id);
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+            if (movie == null)
+                return HttpNotFound();
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+            return View("MovieForm", viewModel);
         }
+
+
         public ViewResult Index()
         {
             var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
         }
+
+
+        public ActionResult New()
+        {
+            var genrer = _context.Genres.ToList();
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genrer
+            };
+            return View("MovieForm",viewModel);
+        }
+
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+                _context.Movies.Add(movie);
+            else
+            {
+                var movieInDb = _context.Movies.Single(c => c.Id == movie.Id);
+                //TryUpdateModel(customerInDb)
+
+                movieInDb.Name = movie.Name;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.DateAdded = movie.DateAdded;
+                movieInDb.NumberInStock = movie.NumberInStock;
+            }
+            _context.SaveChanges();
+
+            //Redirect the User to Index in Customers(Controllers)
+            return RedirectToAction("Index", "Movies");
+        }
+
 
         /* public ActionResult Index(int? pageIndex, string sortBy)
          {
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -10,16 +10,25 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
         [Required]
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+
         [Required]
         public DateTime DateAdded { get; set; }
+
         [Required]
+        [Display(Name="Number in Stock")]
         public byte NumberInStock { get; set; }
+
         public Genre Genre { get; set; }
+        [Display(Name = "Genre")]
+        [Required]
         public int GenreId { get; set; }
 
     }
